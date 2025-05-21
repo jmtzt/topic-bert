d267186e1361617a3ecf2bb5f98f3dccd7004247
jmtzt <jmttozato@gmail.com>
2025-05-21 14:52:20 +0200

    add config for mlflow

--- src/config.py
+++ src/config.py
@@ -4,10 +4,19 @@ import logging.config
 import sys
 from pathlib import Path
 
+import mlflow
+
 # Directories
 ROOT_DIR = Path(__file__).parent.parent.absolute()
 LOGS_DIR = Path(ROOT_DIR, "logs")
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
+EFS_DIR = Path(ROOT_DIR, "efs")
+Path(EFS_DIR).mkdir(parents=True, exist_ok=True)
+
+MODEL_REGISTRY = Path(f"{EFS_DIR}/mlflow")
+Path(MODEL_REGISTRY).mkdir(parents=True, exist_ok=True)
+MLFLOW_TRACKING_URI = "file://" + str(MODEL_REGISTRY.absolute())
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 # Dataset
 DATASET_NAME = "community-datasets/yahoo_answers_topics"
