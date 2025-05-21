282c92bcdd4c762981b561fffca761873ea3cc4f
jmtzt <jmttozato@gmail.com>
2025-05-21 18:18:33 +0200

    add more unit tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,8 @@
+import tempfile
+from pathlib import Path
+
 import numpy as np
+import pytest
 import torch
 from ray.train.torch import get_device
 
@@ -31,3 +35,40 @@ def test_collate_fn():
     }
     for k in batch:
         assert torch.allclose(processed_batch[k], expected_batch[k])
+
+
+def test_set_seeds():
+    """Test that the random seed is set correctly."""
+    utils.set_seeds(42)
+    a = np.random.randn(1, 3)
+    b = np.random.randn(2, 3)
+    utils.set_seeds()
+    x = np.random.randn(1, 3)
+    y = np.random.randn(2, 3)
+    assert np.array_equal(a, x)
+    assert np.array_equal(b, y)
+
+
+def test_save_and_load_dict():
+    """Test that a dictionary can be saved and loaded correctly."""
+    with tempfile.TemporaryDirectory() as dp:
+        d = {"hello": "world"}
+        fp = Path(dp, "d.json")
+        utils.save_dict(d=d, path=fp)
+        d = utils.load_dict(path=fp)
+        assert d["hello"] == "world"
+
+
+@pytest.mark.parametrize(
+    "d, keys, list",
+    [
+        (
+            {"a": [1, 2], "b": [1, 2]},
+            ["a", "b"],
+            [{"a": 1, "b": 1}, {"a": 2, "b": 2}],
+        ),
+        ({"a": [1, 2], "b": [1, 2]}, ["a"], [{"a": 1}, {"a": 2}]),
+    ],
+)
+def test_dict_to_list(d, keys, list):
+    assert utils.dict_to_list(d, keys=keys) == list
